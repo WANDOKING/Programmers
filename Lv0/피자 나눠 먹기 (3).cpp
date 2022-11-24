6fef947613fc70db0be3df02b78c2c18e469b9cf
Dowan Kim <108464645+WANDOKING@users.noreply.github.com>
2022-11-24 12:12:25 +0900

    Create 피자 나눠 먹기 (3).cpp

---
+++ Lv0/피자 나눠 먹기 (3).cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int slice, int n) {
+    int answer = 0;
+    
+    while (slice * answer < n)
+    {
+        answer++;
+    }
+    
+    return answer;
+}

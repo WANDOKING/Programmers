be8f0c690852a5f26d70d4d0a8882d9b6e70b54c
WANDOKING <108464645+WANDOKING@users.noreply.github.com>
2022-12-05 15:30:01 +0900

    Create 특정 문자 제거하기.cpp

---
+++ Lv0/특정 문자 제거하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+    
+    for (char ch : my_string)
+    {
+        if (ch == letter[0])
+        {
+            continue;
+        }
+        
+        answer += ch;
+    }
+    
+    return answer;
+}
